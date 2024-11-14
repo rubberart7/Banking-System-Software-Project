9f56f7686d5aa3737d29b345022b2c557f23bb77
mbyaf <rubberart7@users.noreply.github.com>
2024-11-14 15:24:55 -0500

    start withdraw command processor, can withdraw from checking account

---
+++ src/test/java/WithdrawCommandProcessorTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class WithdrawCommandProcessorTest {
+	CommandProcessor commandProcessor;
+	Bank bank;
+
+	@BeforeEach
+	void setUp() {
+		bank = new Bank();
+		commandProcessor = new CommandProcessor(bank);
+	}
+
+	@Test
+	void withdraw_from_checking_acc_decreases_the_balance_of_the_correct_acc() {
+		commandProcessor.processCommand("create checking 12345678 1.2");
+		commandProcessor.processCommand("deposit 12345678 500");
+		commandProcessor.processCommand("withdraw 12345678 100");
+
+		assertEquals(400, bank.getAccounts().get("12345678").getBalance());
+	}
+}
